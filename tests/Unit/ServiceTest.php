618a43db4ef272641ae0435357a0d235721dc94c
Bereczky Balazs <balazs.bereczky@stilldesign.hu>
2018-09-01 11:48:51 +0200

    api url and key in service

--- tests/Unit/ServiceTest.php
+++ tests/Unit/ServiceTest.php
@@ -1,9 +1,21 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use BereczkyBalazs\BrickCore\Service;
 
 class ServiceTest extends TestCase
 {
+    private $service;
+
+    const TEST_API_KEY = ['key' => 'TEST_API_KEY', 'value' => 'key123'];
+    const TEST_API_URL = ['key' => 'TEST_API_URL', 'value' => 'http://url.com'];
+
+    public function setUp()
+    {
+        $_ENV[self::TEST_API_KEY['key']] = self::TEST_API_KEY['value'];
+        $_ENV[self::TEST_API_URL['key']] = self::TEST_API_URL['value'];
+        $this->service = new Service();
+    }
     /*
      * @test
      */
