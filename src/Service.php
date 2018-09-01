2f6bd4cdd3409faa0fdaf78d96c10aecdfff5121
Bereczky Balazs <balazs.bereczky@stilldesign.hu>
2018-09-01 13:35:47 +0200

    setting up auto api key

--- src/Service.php
+++ src/Service.php
@@ -4,7 +4,44 @@
 namespace BereczkyBalazs\BrickCore;
 
 
-class Service
+abstract class Service
 {
+    const API_KEY_POSTFIX = '_API_KEY';
+    const API_URL_POSTFIX = '_API_URL';
 
+    protected $apiKey;
+
+    public function __construct()
+    {
+        $this->setAutoApiKey();
+    }
+
+    private function setAutoApiKey()
+    {
+        if (!isset($this->apiKey)) {
+            $autoApiKeyIndex = $this->getEnvKeyFromClassName() . self::API_KEY_POSTFIX;
+            if (isset($_ENV[$autoApiKeyIndex])) {
+                $this->setApiKey($_ENV[$autoApiKeyIndex]);
+            }
+        }
+    }
+
+    private function getEnvKeyFromClassName()
+    {
+        $className = get_class($this);
+        $className = str_replace('Service', '', $className);
+        $envIndex = ltrim(strtoupper(preg_replace('/[A-Z]([A-Z](?![a-z]))*/', '_$0', $className)), '_');
+        return $envIndex;
+    }
+
+    public function setApiKey($apiKey)
+    {
+        $this->apiKey = $apiKey;
+        return $this;
+    }
+
+    public function getApiKey()
+    {
+        return $this->apiKey;
+    }
 }
--- tests/Unit/ServiceTest.php
+++ tests/Unit/ServiceTest.php
@@ -5,22 +5,40 @@ use BereczkyBalazs\BrickCore\Service;
 
 class ServiceTest extends TestCase
 {
-    private $service;
-
-    const TEST_API_KEY = ['key' => 'TEST_API_KEY', 'value' => 'key123'];
-    const TEST_API_URL = ['key' => 'TEST_API_URL', 'value' => 'http://url.com'];
+    const TEST_API_KEY = ['key' => 'MOCK_TEST_API_KEY', 'value' => 'key123'];
+    const TEST_API_URL = ['key' => 'MOCK_TEST_API_URL', 'value' => 'http://url.com'];
+    const MOCK_CLASS_NAME = 'MockTestService';
 
     public function setUp()
     {
         $_ENV[self::TEST_API_KEY['key']] = self::TEST_API_KEY['value'];
         $_ENV[self::TEST_API_URL['key']] = self::TEST_API_URL['value'];
-        $this->service = new Service();
     }
+
     /*
      * @test
      */
-    public function test_exists()
+    public function test_setting_api_key()
     {
-        $this->assertTrue(true);
+        $testService = $this->getMockForAbstractClass(
+            Service::class,
+            [],
+            self::MOCK_CLASS_NAME
+        );
+        $apiKey = $testService->setApiKey(self::TEST_API_KEY['value'])->getApiKey();
+        $this->assertEquals(self::TEST_API_KEY['value'], $apiKey);
+    }
+
+    /*
+     * @test
+     */
+    public function test_auto_api_key()
+    {
+        $testService = $this->getMockForAbstractClass(
+            Service::class,
+            [],
+            self::MOCK_CLASS_NAME
+        );
+        $this->assertEquals(self::TEST_API_KEY['value'], $testService->getApiKey());
     }
 }
