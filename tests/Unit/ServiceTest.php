c315eadd2f4bcf20c84ec3526f623de8d2cba7f2
Bereczky Balazs <balazs.bereczky@stilldesign.hu>
2018-09-01 11:37:47 +0200

    tests are initialized

---
+++ tests/Unit/ServiceTest.php
@@ -0,0 +1,14 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class ServiceTest extends TestCase
+{
+    /*
+     * @test
+     */
+    public function test_exists()
+    {
+        $this->assertTrue(true);
+    }
+}
