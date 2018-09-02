a936194531a65ff296187707a1f44596440e0114
Bereczky Balazs <balazs.bereczky@stilldesign.hu>
2018-09-02 23:24:49 +0200

    connection error fixed

--- src/Connection.php
+++ src/Connection.php
@@ -19,6 +19,7 @@ final class Connection
                 Config::getDatabasePassword()
             );
             self::$connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+            self::$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
         return self::$connection;
     }
